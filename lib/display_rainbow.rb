f09cd5b58123dca6c450b673cf1ef945e3d2506a
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-13 08:40:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow(color)
-  puts "Name: "
+  puts "Initial: #{color[0]}, Name: #{color[1]}"
+end
+
+gr
 
 bob     = ["Bob", 46, "Juggler", "555-555-5555"]
 stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
