c6717aa74298d6622bf4d1e915125531776e7b55
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-13 08:35:11 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,6 @@
 # Write your #display_rainbow method here
-def display_rainbow
-  def greeting(name)
-  puts "Hello, #{name}!"
-end
+def display_rainbow(color)
+
 
 bob     = ["Bob", 46, "Juggler", "555-555-5555"]
 stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
